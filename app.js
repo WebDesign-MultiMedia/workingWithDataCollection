8f0f8dbb801741f71bedd0aa69f2063ffc6cec63
j.salas2016 <jsalas198016@gmail.com>
2024-05-21 21:33:12 -0400

    Doing Part One

---
+++ app.js
@@ -0,0 +1,9 @@
+// Objectives
+    //Use Array to store ordered lists of data
+    // use object to stored keyed lists of data
+    // use conditional logic to process data
+    // use loops to handle repetitive task
+    // transform data according to specification
+
+
+// PART ONE Refactoring Old Code
